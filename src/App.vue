d96705052eb665a0f9bc012efa3c86f9dc9ee3f6
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-06-12 20:25:12 +0530

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,82 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="white" elevation="0" class="px-2">
+      <div class="d-flex align-center">
+        <!-- 
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />-->
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+        <v-toolbar-title class="text-h6 pl-2">Portfolio</v-toolbar-title>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        class="text-none"
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span>About</span>
+      </v-btn>
+      <v-btn
+        class="text-none"
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span>Projects</span>
+      </v-btn>
+      <v-btn
+        class="text-none"
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span>Blogs</span>
+      </v-btn>
+      <v-btn
+        class="text-none mr-2"
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span>Contact</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld />
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import HelloWorld from "./components/HelloWorld";
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: "App",
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    HelloWorld
+  },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  })
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,158 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container class="px-8">
+    <v-row class="py-4" align="center">
+      <v-col cols="6">
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to Ayush World!</h1>
+
+        <!-- <p class="subheading font-weight-regular">
+          Hi, I am Ayush P Gupta
+          <br />please join our online
+          <a
+            href="https://community.vuetifyjs.com"
+            target="_blank"
+          >Discord Community</a>
+        </p>-->
+        <p class="text-h5 font-weight-regular grey--text text--darken-1 my-6">
+          Hi, I am
+          <span class="primary--text font-weight-medium">Ayush P Gupta</span>
+          <br />IIT Roorkee | Flutter & Vue Developer | Physics Educator at
+          Unacademy <br />| Flute Player | DIY person
+        </p>
+
+        <div class="mt-12">
+          <v-btn color="primary">
+            <v-icon left class="mr-2" small>mdi-file</v-icon>VIEW MY RESUME
+          </v-btn>
+        </div>
+      </v-col>
+      <v-col cols="6">
+        <v-img
+          alt="Vuetify Name"
+          contain
+          min-width="100"
+          src="../assets/cover.svg"
+        />
+      </v-col>
+
+      <!-- <v-col class="mb-5" cols="12">
+        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(eco, i) in ecosystem"
+            :key="i"
+            :href="eco.href"
+            class="subheading mx-3"
+            target="_blank"
+          >{{ eco.text }}</a>
+        </v-row>
+      </v-col>-->
+    </v-row>
+
+    <v-divider class="my-8"></v-divider>
+
+    <v-row class="text-center py-4" justify="center">
+      <v-col cols="12">
+        <p class="text-h4 font-weight-bold mb-3">About Me</p>
+      </v-col>
+      <v-col cols="12">
+        <p class="text-h6 font-weight-regular grey--text text--darken-1">
+          I am a graduate from IIT Roorkee (2018)<br />
+          I belong to Lucknow, India<br /><br />
+
+          My area of interest is Application Development, Teaching Physics,<br />
+          Playing Flute, Sketching, DIY stuff, self-learning and many more.
+          <br />
+          <br />Mainly my expertise is with Mobile Application Development using
+          <a
+            class=" font-weight-medium px-1"
+            href="https://flutter.dev/"
+            target="_blank"
+          >
+            Flutter</a
+          >SDK.<br />
+          Also I have intermediate expertise with web app development using
+          <a
+            class=" font-weight-medium px-1"
+            href="https://vuejs.org/"
+            target="_blank"
+          >
+            VueJs</a
+          >.
+        </p>
+      </v-col>
+    </v-row>
+
+    <v-divider class="my-8"></v-divider>
+    <v-row class="text-center py-4" justify="center">
+      <v-col cols="12">
+        <p class="text-h4 font-weight-bold mb-3">My Github</p>
+      </v-col>
+      <v-col cols="12">
+        <v-btn color="grey darken-3 text-none" dark>
+          <v-icon left class="mr-3" medium>mdi-github</v-icon>github.com/apgapg
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  name: "HelloWorld",
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    ecosystem: [
+      {
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader",
+      },
+      {
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify",
+      },
+      {
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify",
+      },
+    ],
+    importantLinks: [
+      {
+        text: "Documentation",
+        href: "https://vuetifyjs.com",
+      },
+      {
+        text: "Chat",
+        href: "https://community.vuetifyjs.com",
+      },
+      {
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify",
+      },
+      {
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs",
+      },
+      {
+        text: "Articles",
+        href: "https://medium.com/vuetify",
+      },
+    ],
+    whatsNext: [
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer",
+      },
+      {
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/layout/pre-defined",
+      },
+      {
+        text: "Frequently Asked Questions",
+        href:
+          "https://vuetifyjs.com/getting-started/frequently-asked-questions",
+      },
+    ],
+  }),
+};
+</script>
