9d39845b9ae5d169c0327987f1bfb98a16ef8fdd
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-06-15 14:39:39 +0530

    Update PWA config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ],
-  "publicPath":''
-}
+  transpileDependencies: ["vuetify"],
+  publicPath: "",
+  pwa: {
+    name: "Ayush P Gupta",
+    themeColor: "#2196F3",
+    msTileColor: "#000000",
+  },
+};
