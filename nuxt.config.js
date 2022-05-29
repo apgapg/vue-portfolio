21620c00105d4e1890385b62d4f3c08001dd1d00
Ayush P Gupta <ayushpguptaapg@gmail.com>
2022-05-29 21:10:39 +0530

    fix: update title
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - vue-portfolio',
+    titleTemplate: '%s',
     title: 'Ayush P Gupta',
     htmlAttrs: {
       lang: 'en'
@@ -18,7 +18,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Official Portfolio of Ayush P Gupta' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
